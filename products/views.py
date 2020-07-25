068938a01082f0077fc10b77db89974358b0b9fe
Sara Oliver <s.oliver1@icloud.com>
2020-07-25 00:14:01 +0000

    updated products files

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 from django.db.models.functions import Lower
 
 from .models import Product, Category
-from .forms import ProductForm
+# from .forms import ProductForm
 
 
 # Create your views here.
