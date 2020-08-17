bf3a71392ec25476aa8b65e8fd518a900f604da7
Sara Oliver <s.oliver1@icloud.com>
2020-08-17 12:52:50 +0000

    update login security message

--- products/views.py
+++ products/views.py
@@ -76,7 +76,7 @@ def product_detail(request, product_id):
 def add_product(request):
     """ Add a product to the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
+        messages.error(request, 'Sorry not allowed - authorised personel only.')
         return redirect(reverse('home'))
 
     if request.method == 'POST':
@@ -102,7 +102,7 @@ def add_product(request):
 def edit_product(request, product_id):
     """ Edit a product in the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
+        messages.error(request, 'Sorry not allowed - authorised personel only ')
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
@@ -131,7 +131,7 @@ def edit_product(request, product_id):
 def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
+        messages.error(request, 'Sorry not allowed - authorised personel only.')
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
