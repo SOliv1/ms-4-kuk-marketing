1f8673bab1dda627b2f4b8853100c7c2fdc531a9
Sara Oliver <s.oliver1@icloud.com>
2020-09-02 21:12:11 +0000

    add ourservicesto mainmenu

--- services/urls.py
+++ services/urls.py
@@ -1,7 +1,12 @@
-#from django.urls import path
-#from . import views
+from django.urls import path 
+from . import views
 
 
-#urlpatterns = [
-#    path('', views.about, name='about')
-#   ]
+urlpatterns = [
+    path('services/', views.services, name='services'),
+    path('services/about', views.about, name='about'),
+    path('services/our_brands', views.our_brands, name='our_brands'),
+    path('services/contact_us', views.about, name='contact_us'),
+  ]
+
+
--- services/views.py
+++ services/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 
 
-def index(request):
+def services(request):
+    """ A view to all services page """
+
+    return render(request, '/services.html')
+
+
+def about(request):
     """ view to return the about page """
 
     return render(request, 'services/about.html')
 
 
+def our_brands(request):
+    """ A view to our_brands page """
 
+    return render(request, 'services/our_brands.html')
+
+
+def contact_us(request):
+    """ A view to all services page """
+
+    return render(request, 'services/contact_us.html')
