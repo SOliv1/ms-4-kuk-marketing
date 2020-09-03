55d442c54b8cab68e647be1c82592e84dd2deab9
Sara Oliver <s.oliver1@icloud.com>
2020-09-03 20:23:41 +0000

    commit

--- kuk_marketing/settings.py
+++ kuk_marketing/settings.py
@@ -193,7 +193,7 @@ if 'USE_AWS' in os.environ:
 
     #  Config Bucket
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'ms-4-kuk-marketing'
+    AWS_STORAGE_BUCKET_NAME = 'kuk-milestone-4'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
