4c4b2d4dfb07fd98109622a8565a7e4f4258c8b8
Sara Oliver <s.oliver1@icloud.com>
2020-09-06 23:20:28 +0000

    add brands and services templates

--- services/urls.py
+++ services/urls.py
@@ -3,7 +3,19 @@ from . import views
 
 
 urlpatterns = [
-    path('/about/', views.about, name='about'),
-    path('/our_brands/', views.our_brands, name='our_brands'),
-    path('/contact_us/', views.about, name='contact_us.html'),
+
+    path('services/about', views.about, name='about'),
+    path('services/our_brands', views.our_brands, name='our_brands'),
+    path('services/contact_us', views.contact_us, name='contact_us'),
+    path('services/our_services', views.our_services, name='our_services'),
+    path('services/brands', views.brands, name='brands'),
+    path('services/contact', views.contact, name='contact'),
+    path('services/graphic_design',
+         views.graphic_design, name='graphic_design'),
+    path('services/web_design',
+         views.web_design, name='web_design'),
+    path('services/web_devlepment',
+         views.web_development, name='web_development'),
+    path('services/digital_marketing',
+         views.digital_marketing, name='digital_marketing'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 
-def services(request):
-    """ A view to show individual product details """
+""" view-link to return our services menu on main-nav bar"""
 
-    return render(request, 'services/', )
+
+def our_services(request):
+    """ A view to our services page """
+
+    return render(request, 'services/our_services.html')
+
+
+def brands(request):
+    """ A view to brands page """
+
+    return render(request, 'services/brands.html')
+
+
+def contact(request):
+    """ A view to contact page """
+
+    return render(request, 'services/contact.html')
+
+
+""" view-link to return about us menu on main-nav bar"""
 
 
 def about(request):
@@ -20,6 +38,34 @@ def our_brands(request):
 
 
 def contact_us(request):
-    """ A view to all services page """
+    """ A view to contact us page """
 
     return render(request, 'services/contact_us.html')
+
+
+""" view-link to return about us menu on main-nav bar"""
+
+
+def graphic_design(request):
+    """ A view to graphic design page """
+
+    return render(request, 'services/graphic_design.html')
+
+
+def web_design(request):
+    """ A view to web design page """
+
+    return render(request, 'services/web_design.html')
+
+
+def web_development(request):
+    """ A view to web developmnet page """
+
+    return render(request, 'services/web_development.html')
+
+
+def digital_marketing(request):
+    """ A view to digital marketing page """
+
+    return render(request, 'services/digital_marketing.html')
+
