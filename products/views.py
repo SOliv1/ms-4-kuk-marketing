d65c905637b7704d561bfd68ee224de6ce39b0cb
Sara Oliver <s.oliver1@icloud.com>
2020-09-28 12:17:22 +0000

    changes to category names

--- products/views.py
+++ products/views.py
@@ -88,7 +88,7 @@ def add_product(request):
             messages.success(request, 'Successfully added product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(request, 'Failed to add product. Please ensure form is valid.')
     else:
         form = ProductForm()
 
@@ -105,7 +105,7 @@ def edit_product(request, product_id):
     """ Edit a product in the store """
     if not request.user.is_superuser:
         messages.error(request,
-                       'Sorry not allowed - authorised personel only ')
+                       'Sorry - authorised personel only ')
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
@@ -134,7 +134,7 @@ def edit_product(request, product_id):
 def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry not allowed - authorised personel only.')
+        messages.error(request, 'Sorry - authorised personel only.')
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
