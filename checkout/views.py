0112eb7f51fd9f61df23a6acb13f38e278e1b559
Sara Oliver <s.oliver1@icloud.com>
2020-10-01 16:46:43 +0000

    readd the stripe and checkout pages

--- checkout/views.py
+++ checkout/views.py
@@ -106,7 +106,6 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        # Attempt to prefill the form with any info the user maintains in their profile
         if request.user.is_authenticated:
             try:
                 profile = UserProfile.objects.get(user=request.user)
